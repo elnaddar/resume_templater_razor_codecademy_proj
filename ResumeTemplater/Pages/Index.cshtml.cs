c68f6cc51358ab069ca0a909b44772178ce430a3
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-03-31 19:12:42 +0200

    starter project.

---
+++ ResumeTemplater/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+// Index.cshtml.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ResumeTemplater.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+    }
+}
---
+++ ResumeTemplater/Pages/Projects.cshtml.cs
@@ -0,0 +1,41 @@
+// Projects.cshtml.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ResumeTemplater.Models;
+
+namespace ResumeTemplater.Pages
+{
+    public class ProjectsModel : PageModel
+    {
+
+        public List<Project>? Projects { get; set; }
+
+        public void OnGet()
+        {
+            Projects = new List<Project> {
+                new Project()
+                {
+                    Title = "Explorella",
+                    Description = "A large scale travelling application built to help plan your destination and itinerary on your next trip! Built on React, Node, Express, and MongoDB.",
+                    Year = 2019
+                },
+                new Project()
+                {
+                    Title = "Cypher Clothing",
+                    Description = "A large scale e-commerce clothing store application. Built with Razor Pages and SQLite.",
+                    Year = 2019
+                },
+                new Project()
+                {
+                    Title = "Password Box",
+                    Description = "A password manager Chrome plugin used to create and safely store all your passwords! Built with Javascript and HTML.",
+                    Year = 2020
+                }
+            };
+        }
+    }
+}
